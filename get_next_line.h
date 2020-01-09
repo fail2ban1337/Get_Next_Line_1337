07034696ab5f7c81b8baf12ead2ac14835f2c3c7
Administrator <admin@example.com>
2020-01-09 17:53:26 +0100

    get_next_line

---
+++ get_next_line.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abelomar <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/10/26 19:58:24 by abelomar          #+#    #+#             */
+/*   Updated: 2018/10/31 20:19:16 by abelomar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# define BUFF_SIZE 32
+# include "./libft/libft.h"
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <sys/uio.h>
+# include <sys/types.h>
+
+int		get_next_line(const int fd, char **line);
+
+#endif
